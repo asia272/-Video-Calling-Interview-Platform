afb50266d33d685626e1a9bd930dc18235f88aa8
asia272 <asiaashraf7272@gmail.com>
2026-04-16 14:23:24 +0500

    routing initilize

---
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Meeting page is here</div>
+  )
+}
+
+export default page
---
+++ src/app/(root)/recordings/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Recoding page </div>
+  )
+}
+
+export default page
