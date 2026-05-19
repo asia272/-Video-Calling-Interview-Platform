43eccde426173dcf145a1f891c92d7ba364f98cf
asia272 <asiaashraf7272@gmail.com>
2026-05-19 09:34:34 +0500

    add another layout for video stream

--- src/components/providers/StreamClientProvider.tsx
+++ src/components/providers/StreamClientProvider.tsx
@@ -6,7 +6,7 @@ import { useUser } from "@clerk/nextjs";
 import LoaderUI from "../LoaderUI";
 import { streamTokenProvider } from "@/actions/stream.actions";
 
-const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+const StreamClientProvider = ({ children }: { children: ReactNode }) => {
     const [streamVideoClient, setStreamVideoClient] = useState<StreamVideoClient>();
     const { user, isLoaded } = useUser();
 
@@ -31,4 +31,4 @@ const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
     return <StreamVideo client={streamVideoClient}>{children}</StreamVideo>;
 };
 
-export default StreamVideoProvider;
+export default StreamClientProvider;
