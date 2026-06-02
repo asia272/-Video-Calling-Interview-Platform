5117420272653fb5e25728ed9b78bb96846602ad
asia272 <asiaashraf7272@gmail.com>
2026-06-02 16:54:46 +0500

    complete comment sectiong

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -15,6 +15,7 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/componen
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { CalendarIcon, CheckCircle2Icon, ClockIcon, XCircleIcon } from 'lucide-react';
 import { format } from "date-fns";
+import CommentDialog from '@/components/CommentDialog';
 
 type Interview = Doc<"interviews">
 
@@ -111,7 +112,7 @@ const DashboardPage = () => {
                             </Button>
                           </div>
                         )}
-                        {/* <CommentDialog interviewId={interview._id} /> */}
+                        <CommentDialog interviewId={interview._id} />
                       </CardFooter>
                     </Card>
                   );
