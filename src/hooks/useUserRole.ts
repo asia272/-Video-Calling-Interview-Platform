8d415b14b85df4d154b21cca7b3d0b8575d9906e
asia272 <asiaashraf7272@gmail.com>
2026-06-04 11:41:36 +0500

    debugging

--- src/hooks/useUserRole.ts
+++ src/hooks/useUserRole.ts
@@ -5,17 +5,10 @@ import { api } from "../../convex/_generated/api";
 export const useUserRole = () => {
     const { user, isLoaded } = useUser();
 
-
-
-
-
     const userData = useQuery(api.users.getUserByClerkId, {
         clerkId: user?.id || "",
     })
 
-    console.log("isLoaded:", isLoaded);
-    console.log("user:", user);
-    console.log("user id:", user?.id);
     return {
         isLoading: userData === undefined,
         isInterviewer: userData?.role === "interviewer",
